e873c590799e51fc61f158f38d3f649bbf3a504f
ms747 <mayur.shah.ha@gmail.com>
2021-05-03 18:15:09 +0530

    Wireguard CLI

---
+++ cli2.py
@@ -0,0 +1,88 @@
+import json
+import subprocess
+
+class Client:
+    # Client variables
+    name        = None
+    private_key = None
+    public_key  = None
+    cidr          = None
+
+    # Client Constructor
+    def __init__(self, name, private_key, public_key, cidr):
+        self.name        = name
+        self.private_key = private_key
+        self.public_key  = public_key
+        self.cidr          = cidr
+
+class Server:
+    # Server variables
+    clients     = []
+    cidr        = None
+    port        = None
+    name        = None
+    private_key = None
+    public_key  = None
+    path        = None
+    public_ip   = None
+
+    # Create private/public key pairs
+    def create_keys(self):
+        private_key = subprocess.run(["wg", "genkey"], encoding="utf-8", stdout=subprocess.PIPE).stdout.strip()
+        public_key  = subprocess.run(["wg", "pubkey"], encoding="utf-8", stdout=subprocess.PIPE, input=private_key).stdout.strip()
+        return (private_key, public_key)
+
+    # Create keys for server and set the interface
+    def generate_server_config(self, path, interface):
+        self.name = interface
+        self.private_key, self.public_key = self.create_keys()
+
+    # Add Client/Peer to the Server
+    def add_client(self, name, cidr):
+        private, public = self.create_keys()
+        self.clients.append(Client(name, private, public, cidr).__dict__)
+
+    # Save the config
+    def save_config(self):
+        server = { "name": self.name,
+                   "public_ip": self.public_ip,
+                   "private_key": self.private_key,
+                   "public_key": self.public_key,
+                   "cidr": self.cidr,
+                   "port": self.port,
+                   "clients": self.clients }
+        with open(self.path, "w") as config:
+          json.dump(server, config, indent=4)
+
+    # Server constructor
+    def __init__(self, public_ip, cidr, port, interface, path):
+        self.path = path
+        self.cidr = cidr
+        self.port = port
+        self.public_ip = public_ip
+        # Check if file exists
+        print("Generating...")
+        self.generate_server_config(self.path, interface)
+
+def main():
+    print("WG CLI 0.1")
+    public_ip = input("Enter Public IP : ")
+    interface = input("Enter interface name : ")
+    cidr = input("Enter interface cidr (e.g ip/netmask) : ")
+    port = input("Enter port : ")
+
+    server = Server(public_ip, cidr, port, interface, f"{interface}-config.json")
+
+    while True:
+        client_name = input("Enter client name : ")
+        client_cidr = input("Enter client cidr : ")
+        server.add_client(client_name, client_cidr)
+        new_client  = input("Press q to quit : ")
+
+        if new_client == "q":
+            break
+
+    server.save_config()
+
+if __name__ == "__main__":
+    main()
---
+++ generator.py
@@ -0,0 +1,58 @@
+import json
+import sys
+import subprocess
+
+# Generates Server's Config
+def server_config(server):
+    client_string = ""
+    for client in server["clients"]:
+        client_string +=  "\n[Peer]\n"
+        client_string += f"PublicKey = {client['public_key']}\n"
+        client_string += f"AllowedIPs = {client['cidr']}\n"
+
+
+    output = "[Interface]\n" \
+            f"Address = {server['cidr']}\n"\
+            f"ListenPort = {server['port']}\n"\
+            f"PrivateKey = {server['private_key']}\n"\
+            "MTU = 1450\n"\
+            f"PostUp = iptables -A FORWARD -i {server['name']} -j ACCEPT; iptables -t nat -A POSTROUTING -o {server['name']} -j MASQUERADE\n"\
+            f"PostDown = iptables -D FORWARD -i {server['name']} -j ACCEPT; iptables -t nat -D POSTROUTING -o {server['name']} -j MASQUERADE\n"\
+            f"{client_string}"
+    return output
+
+# Generates Client Configs
+def client_config(server):
+    client_configs = []
+    for client in server['clients']:
+        output = f"# Config for {client['name']}\n"\
+                 f"[Interface]\n" \
+                 f"Address = {client['cidr']}\n"\
+                 f"PrivateKey = {client['private_key']}\n"\
+                 f"DNS = 1.1.1.1\n"\
+                 f"\n[Peer]\n"\
+                 f"PublicKey = {server['public_key']}\n"\
+                 f"AllowedIPs = {client['cidr']}\n"\
+                 f"Endpoint = {server['public_ip']}:{server['port']}\n"\
+                 f"PersistentKeepalive = 15\n"
+        client_configs.append(output)
+    return client_configs
+
+
+def main():
+    config = sys.argv[1]
+    with open(config) as f:
+        data = json.load(f)
+        # Create Server Config
+        generated_server_config = server_config(data)
+        print(generated_server_config)
+        print()
+
+        # Create Clients Config
+        generated_clients_config = client_config(data)
+        for config in generated_clients_config:
+            out = subprocess.run(["qr", config])
+            print(config)
+
+if __name__ == "__main__":
+    main()
